a04b48b213c5636cd4d8837210bc36f80ebe7e9e
Redmond Chan <redmondchan92@gmail.com>
2019-01-07 04:12:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,13 @@ end
 
 
 def get_japanese_emoticon
-  # code goes here
+  emoticons = load_library(path)
+  result = emoticons["get_emoticon"][emoticon]
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
 
 def get_english_meaning
